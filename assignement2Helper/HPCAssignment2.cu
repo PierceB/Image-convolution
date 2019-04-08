83a1661dd3c4e2d1bcf34c538ce3b6b91e5d7d64
PierceB <vmuser@host109-03-02.ms.wits.ac.za>
2019-04-08 11:57:19 +0200

    added hpcassignment file

---
+++ assignement2Helper/HPCAssignment2.cu
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef _WIN32
+#  define WINDOWS_LEAN_AND_MEAN
+#  define NOMINMAX
+#  include <windows.h>
+#endif
+
+// Includes CUDA
+#include <cuda_runtime.h>
+
+// Utilities and timing functions
+#include <./helper_functions.h>    // includes cuda.h and cuda_runtime_api.h
+
+// CUDA helper functions
+#include <helper_cuda.h>         // helper functions for CUDA error check
+
+#define MAX_EPSILON_ERROR 5e-3f
+
+const char *imageFilename = "lena_bw.pgm";
+const char *refFilename   = "ref_rotated.pgm";
+
+const char *sampleName = "HPCAssignment2.cu";
+
+
+
+
+
+
+int main(void){ 
+
+}
+
